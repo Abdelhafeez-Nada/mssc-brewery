41ad16effb457966d390523461f19471b318e838
Abdelhafeez <abdelhafeez0nada@gmail.com>
2024-03-18 04:36:24 +0200

    create beer controller

---
+++ mssc-brewery/src/main/java/org/abdelhafeez/msscbrewery/web/controller/BeerController.java
@@ -0,0 +1,27 @@
+package org.abdelhafeez.msscbrewery.web.controller;
+
+import java.util.UUID;
+
+import org.abdelhafeez.msscbrewery.service.BeerService;
+import org.abdelhafeez.msscbrewery.web.dto.BeerDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/api/v1/beer")
+@AllArgsConstructor
+public class BeerController {
+
+    private final BeerService beerService;
+
+    @GetMapping("/{beerId}")
+    public ResponseEntity<BeerDto> findById(@PathVariable UUID beerId) {
+        return new ResponseEntity<BeerDto>(beerService.getBeerById(beerId), HttpStatus.OK);
+    }
+}
