e47639d97203c1c4e9a74e447ea0657e5c7f1ee4
Abdelhafeez <abdelhafeez0nada@gmail.com>
2024-03-18 04:10:59 +0200

    init

---
+++ mssc-brewery/src/test/java/org/abdelhafeez/msscbrewery/MsscBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.abdelhafeez.msscbrewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBreweryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
