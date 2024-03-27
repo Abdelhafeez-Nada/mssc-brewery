3bb57b2ea6dcb41a104ef0d441b0884e6abd5876
Abdelhafeez <abdelhafeez0nada@gmail.com>
2024-03-27 10:39:45 +0200

    add crud methods to customer service

--- mssc-brewery/src/main/java/org/abdelhafeez/msscbrewery/service/CustomerService.java
+++ mssc-brewery/src/main/java/org/abdelhafeez/msscbrewery/service/CustomerService.java
@@ -7,4 +7,10 @@ import org.abdelhafeez.msscbrewery.web.dto.CustomerDto;
 public interface CustomerService {
 
     public CustomerDto getCustomerById(UUID id);
+
+    public CustomerDto saveNewCustomer(CustomerDto dto);
+
+    public void updateCustomer(UUID id, CustomerDto dto);
+
+    public void deleteById(UUID id);
 }
