72fbbfa7f1af23c67b69c8e37fb4ddbe68214e95
Abdelhafeez <abdelhafeez0nada@gmail.com>
2024-03-27 10:40:47 +0200

    implement crud APIs in customer controller

--- mssc-brewery/src/main/java/org/abdelhafeez/msscbrewery/web/controller/CustomerController.java
+++ mssc-brewery/src/main/java/org/abdelhafeez/msscbrewery/web/controller/CustomerController.java
@@ -4,14 +4,20 @@ import java.util.UUID;
 
 import org.abdelhafeez.msscbrewery.service.CustomerService;
 import org.abdelhafeez.msscbrewery.web.dto.CustomerDto;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @RequestMapping("api/v1/customer")
 @RestController
@@ -23,5 +29,25 @@ public class CustomerController {
     @GetMapping("/{customerId}")
     public ResponseEntity<CustomerDto> getCustomerById(@PathVariable("customerId") UUID id) {
         return new ResponseEntity<CustomerDto>(customerService.getCustomerById(id), HttpStatus.OK);
+    }
+
+    @PostMapping("")
+    public ResponseEntity<Void> createCustomer(@RequestBody CustomerDto customerDto) {
+        CustomerDto saved = customerService.saveNewCustomer(customerDto);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", "api/v1/customer/" + saved.getId().toString());
+        return new ResponseEntity<>(headers, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{customerId}")
+    public ResponseEntity<Void> updateCustomer(UUID customerId, @RequestBody CustomerDto customerDto) {
+        customerService.updateCustomer(customerId, customerDto);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping("/{customerId}")
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteCustomer(@PathVariable("customerId") UUID id) {
+        customerService.deleteById(id);
     }
 }
